26a7d3849a3771c80ca8a63976f08d548c5e94bb
G'lek Tarssza <glek@glektarssza.com>
2024-03-01 09:15:45 -0700

    Moved over to Jest for now.

--- tests/dummy.spec.ts
+++ tests/dummy.spec.ts
@@ -1,3 +1,13 @@
+import {hello} from '../src/ts/dummy';
+
 describe('A dummy test', () => {
-    it('should pass', () => {});
+    it('should pass', () => {
+        //-- Given
+
+        //-- When
+        const r = hello();
+
+        //-- Then
+        expect(r).toEqual('Hello world!');
+    });
 });
